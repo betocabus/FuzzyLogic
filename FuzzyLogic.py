2455f838083c8ca5a07c8e992326602e98b25fae
Alberto Cabús <betocabus@gmail.com>
2018-06-12 05:02:27 -0300

    change set value to .at in PANDAS

--- FuzzyLogic.py
+++ FuzzyLogic.py
@@ -14,23 +14,6 @@ def aggMemberFunc(salarioVal, tempoSVal, patrimonioVal):
 
     #pertinência
     salario_Mbaixo = fuzz.trapmf(salario, [0, 0, 800, 1000])
-    salario_baixo = fuzz.trapmf(salario, [800, 1000, 2000, 3500])
-    salario_medio = fuzz.trapmf(salario, [2000, 3500, 5500, 7000])
-    salario_alto = fuzz.trapmf(salario, [5500, 7000, 100000, 1000000])
-
-    tempo_pouco = fuzz.trapmf(tempoS, [0, 0, 3, 5])
-    tempo_moderado = fuzz.trapmf(tempoS, [3, 7, 15, 20])
-    tempo_muito = fuzz.trapmf(tempoS, [15, 30, 100, 100])
-
-    patrimonio_pequeno = fuzz.trapmf(patrimonio, [0, 0, 15000, 35000])
-    patrimonio_medio = fuzz.trapmf(patrimonio, [15000, 35000, 400000, 600000])
-    patrimonio_grande = fuzz.trapmf(patrimonio, [400000, 800000, 10000000, 10000000])
-
-    incomp_baixa = fuzz.trapmf(incomp, [0, 0, 15, 35])
-    incomp_media = fuzz.trapmf(incomp, [30, 40, 60, 70])
-    incomp_alta = fuzz.trapmf(incomp, [60, 80, 100, 100])
-    #pertinência
-    salario_Mbaixo = fuzz.trapmf(salario, [0, 0, 800, 1000])
     salario_baixo = fuzz.gaussmf(salario, 2200,1000)
     salario_medio = fuzz.gaussmf(salario, 4000,1500)
     salario_alto = fuzz.trapmf(salario, [5500, 7000, 100000, 100000])
@@ -113,23 +96,25 @@ def aggMemberFunc(salarioVal, tempoSVal, patrimonioVal):
     final_rule = np.fmax(rule0, np.fmax(rule1, np.fmax(rule2, np.fmax(rule3, np.fmax(rule4, np.fmax(rule5, rule6))))))
 
     incomp0 = rule0 * incomp_baixa
-    incomp1 = rule1 * incomp_baixa
+    incomp1 = rule1 * incomp_alta
     incomp2 = rule2 * incomp_media
     incomp3 = rule3 * incomp_media
     incomp4 = rule4 * incomp_alta
-    incomp5 = rule5 * incomp_alta
+    incomp5 = rule5 * incomp_baixa
     incomp6 = rule6 * incomp_alta
 
     final_rule2 = np.fmax(incomp0, np.fmax(incomp1, np.fmax(incomp2, np.fmax(incomp3, np.fmax(incomp4, np.fmax(incomp5, incomp6))))))
 
 
     incompat = fuzz.defuzz(incomp, final_rule2, 'centroid')
+    #print("\nRule0: " + str(rule0))
+    #print("\nRule1: " + str(rule1))
+    #print("\nRule2: " + str(rule2))
+    #print("\nRule3: " + str(rule3))
+    #print("\nRule4: " + str(rule4))
+    #print("\nRule5: " + str(rule5))
+    #print("\nRule6: " + str(rule6))
     return incompat
-
-
-
-
-
 
 #fim fuzzy logic
 
@@ -137,12 +122,13 @@ def aggMemberFunc(salarioVal, tempoSVal, patrimonioVal):
 df = pd.read_csv("group5.csv", header=0, sep=",")
 
 linhas = df.shape[0]
-for x in range(0, 5):
+for x in range(0, 15):
     g = aggMemberFunc(df.valor_bruto_mensal_para_o_mes_de_ref[x], df.data_cargo[x], df.valor_venal[x])
 
     print("Servidor: " + str(df.hash_cpf[x]) + "       Incompatibilidade: " + str(g))
 
+    #df2 = df.set_value(x, 'incompatibilidade_servidor', g, takeable=False)
 
-    df2 = df.set_value(x, 'incompatibilidade_servidor', g, takeable=False)
+    df.at[x, 'incompatibilidade_servidor'] = g
 
-df2.to_csv("group6.csv", sep=",", index=False, columns=['hash_cpf', 'valor_venal','carga_horaria_semanal','data_cargo','valor_bruto_mensal_para_o_mes_de_ref','incompatibilidade_servidor'])
+df.to_csv("group6.csv", sep=",", index=False, columns=['hash_cpf', 'valor_venal','carga_horaria_semanal','data_cargo','valor_bruto_mensal_para_o_mes_de_ref','incompatibilidade_servidor'])
