c3075bb5e2153d39b658f7465d3b2671ad7df224
Alberto Cabús <betocabus@gmail.com>
2018-06-11 21:57:54 -0300

    correcting syntax errors

--- FuzzyLogic.py
+++ FuzzyLogic.py
@@ -43,39 +43,39 @@ incomp_alta = fuzz.trapmf(incomp, [60, 80, 100, 100])
 
 
 
-def aggMemberFunc (salarioVal, tempoSVal, patrimonioVal, salario, tempoS, patrimonio, salario_Mbaixo, salario_baixo, salario_medio, salario_alto, tempo_pouco, tempo_moderado, tempo_muito, patrimonio_pequeno, patrimonio_medio, patrimonio_grande, incomp, incomp_baixa, incomp_media, incomp_alta)
+def aggMemberFunc(salarioVal, tempoSVal, patrimonioVal, salario, tempoS, patrimonio, salario_Mbaixo, salario_baixo, salario_medio, salario_alto, tempo_pouco, tempo_moderado, tempo_muito, patrimonio_pequeno, patrimonio_medio, patrimonio_grande, incomp, incomp_baixa, incomp_media, incomp_alta):
 #interpola as variáveis
-salario0 = fuzz.interp_membership(salario, salario_Mbaixo)
-salario1 = fuzz.interp_membership(salario, salario_baixo)
-salario2 = fuzz.interp_membership(salario, salario_medio)
-salario3 = fuzz.interp_membership(salario, salario_alto)
-tempo0 = fuzz.interp_membership(tempoS, tempo_pouco)
-tempo1 = fuzz.interp_membership(tempoS, tempo_moderado)
-tempo2 = fuzz.interp_membership(tempoS, tempo_muito)
-patrimonio0 = fuzz.interp_membership(patrimonio, patrimonio_pequeno)
-patrimonio1 = fuzz.interp_membership(patrimonio, patrimonio_medio)
-patrimonio2 = fuzz.interp_membership(patrimonio, patrimonio_grande)
-# Determina os pesos para cada antecedente
-rule0 = np.fmax(salario0, tempo0) #Salario muito baixo e pouco tempo servico
-rule1 = np.fmax(salario0, tempo0) #Salario muito baixo e pouco tempo servico
+    salario0 = fuzz.interp_membership(salario, salario_Mbaixo)
+    salario1 = fuzz.interp_membership(salario, salario_baixo)
+    salario2 = fuzz.interp_membership(salario, salario_medio)
+    salario3 = fuzz.interp_membership(salario, salario_alto)
+    tempo0 = fuzz.interp_membership(tempoS, tempo_pouco)
+    tempo1 = fuzz.interp_membership(tempoS, tempo_moderado)
+    tempo2 = fuzz.interp_membership(tempoS, tempo_muito)
+    patrimonio0 = fuzz.interp_membership(patrimonio, patrimonio_pequeno)
+    patrimonio1 = fuzz.interp_membership(patrimonio, patrimonio_medio)
+    patrimonio2 = fuzz.interp_membership(patrimonio, patrimonio_grande)
+    # Determina os pesos para cada antecedente
+    rule0 = np.fmax(salario0, tempo0) #Salario muito baixo e pouco tempo servico
+    rule1 = np.fmax(salario0, tempo0) #Salario muito baixo e pouco tempo servico
 
 
 #fim fuzzy logic
 
 
-df = pd.read_csv("group4.csv", header=0, sep=";", converters={'data_cargo':convert_data_cargo, 'valor_bruto_mensal_para_o_mes_de_ref':convert_comma_dot})
+#df = pd.read_csv("group4.csv", header=0, sep=";", converters={'data_cargo':convert_data_cargo, 'valor_bruto_mensal_para_o_mes_de_ref':convert_comma_dot})
 
 #df.to_csv("servidores.csv", sep=",", index=False, columns=['hash_cpf', 'carga_horaria_semanal','data_cargo','valor_bruto_mensal_para_o_mes_de_ref'])
 
 
 #servidor = df[df.valor_bruto_mensal_para_o_mes_de_ref>=20000]
 
-servidor = df
+#servidor = df
 
 #servidor = str(df['valor_bruto_mensal_para_o_mes_de_ref']).replace(",", ".")
 
-rows, columns = df.shape
+#rows, columns = df.shape
 
 #servidor.to_csv("servidor.csv", sep=",", index=False, columns=['hash_cpf', 'carga_horaria_semanal','data_cargo','valor_bruto_mensal_para_o_mes_de_ref'])
 
-print (servidor)
+#rint (servidor)
